5c073e99ccb5d936eac062b7758ed9784e078d2d
Paul Fielek <paulfielek@gmail.com>
2015-05-29 09:43:01 -0400

    user page complete

--- lemonDrop/app/controllers/users_controller.rb
+++ lemonDrop/app/controllers/users_controller.rb
@@ -11,25 +11,32 @@ class UsersController < ApplicationController
     # This allows us to use Oath authenication
     keys = Rails.application.secrets
     # Refencing the secrets
-    client = Twitter::REST::Client.new do |config|
+    @client = Twitter::REST::Client.new do |config|
       config.consumer_key = keys[:twitter_key]
       config.consumer_secret = keys[:twitter_secret]
       config.access_token = keys[:access_token]
       config.access_token_secret = keys[:access_secret]
     end
     # passing through the information about the user and also our authenticity token and form
-    @controller = {
-      :user => @user,
-      :form => {
-        :action => user_path,
-        :csrf_param => request_forgery_protection_token,
-        :csrf_token => form_authenticity_token
+    if request.xhr?
+      @controller = {
+        :user => @user,
+        :form => {
+          :action => user_path,
+          :csrf_param => request_forgery_protection_token,
+          :csrf_token => form_authenticity_token
+        }
       }
-    }
+      render :json => @controller
+    else
+      @controller = {
+        :user => @user, :form => {:action => user_path}
+      }
+    end
   end
   # Create a new user profile and login
   def create
-    @user = User.create(user_params)
+    @user = User.create(create_params)
     if @user.save
       session[:user_id] = @user.id
       redirect_to @user
@@ -39,10 +46,21 @@ class UsersController < ApplicationController
     end
   end
 
+  # Update a user and return the updated user
+  def update
+    @user = User.find(session[:user_id])
+    @user.update(user_params)
+    render :json => @user
+  end
+
   # protects things being entered into the database
   private
+  def create_params
+    params.require(:user).permit(:first, :last, :email, :password, :confirm_password)
+  end
+
   def user_params
-    params.require(:user).permit(:first, :last, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first, :last, :email, :location, :picture, :github, :linkedin, :facebook, :quote, :bio, :skill_primary, :skill_secondary, :personal_link, :twitter_handle)
   end
 
 end
