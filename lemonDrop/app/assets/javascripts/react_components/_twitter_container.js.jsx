850bd877c385ed6ea2f8505355508abba254644c
Molly Setzer <molly.setzer@gmail.com>
2015-06-01 11:21:01 -0400

    css for profile

--- lemonDrop/app/assets/javascripts/react_components/_twitter_container.js.jsx
+++ lemonDrop/app/assets/javascripts/react_components/_twitter_container.js.jsx
@@ -1,38 +1,34 @@
 var TwitterContainer = React.createClass ({
-	getInitialState: function(){
-		return JSON.parse(this.props.controller);
-	},
+    getInitialState: function(){
+        return JSON.parse(this.props.controller);
+    },
 
-	render: function() {
-		return (
-			<div className = "tweets">
-				<UserTweets tweets={this.state.tweets} />
-			</div>
-		);
-	}
+    render: function() {
+        return (
+            <div className="tweets">
+                <UserTweets tweets={this.state.tweets} />
+            </div>
+        );
+    }
 });
 // shows the tweets of appropriate person
 var UserTweets = React.createClass({
-	render: function() {
-		tweetNodes = this.props.tweets.map(function(tweet){
-			return <Tweet tweet={tweet} />
-		});
+    render: function() {
+        tweetNodes = this.props.tweets.map(function(tweet){
+            return <Tweet tweet={tweet} />
+        });
 
-		return (
-			<div>
-			{tweetNodes}
-			</div>
-			);
-	}
+        return <div>{tweetNodes}</div>
+    }
 });
 // gets tweet
 var Tweet = React.createClass({
-	render: function() {
-		return (
-			<div>
-			<p>{this.props.tweet.text}</p>
-			<i>{this.props.tweet.created_at}</i>
-			</div>
-			);
-	}
+    render: function() {
+        return (
+            <div>
+                <p>{this.props.tweet.text}</p>
+                <i>{this.props.tweet.created_at}</i>
+            </div>
+        );
+    }
 });
